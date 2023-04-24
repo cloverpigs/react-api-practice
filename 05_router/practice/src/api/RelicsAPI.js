356092361bf897531305c716be57eb69b8de58cb
byoungoh.kang <byoungoh.kang@gmail.com>
2023-04-24 22:18:27 +0900

    commit

--- 05_router/practice/src/api/RelicsAPI.js
+++ 05_router/practice/src/api/RelicsAPI.js
@@ -5,9 +5,9 @@ const API_KEY = 'a82a6c95-cada-4967-95a6-f930866aa358';
 export async function getRelicsList(){
     const url = `http://api.kcisa.kr/API_CNV_048/request?serviceKey=${API_KEY}`;
 
-    const response = await fetch(url, { 'Content-type' : 'application/json' });;
+    const response = await fetch(url, {headers : { 'Content-type' : 'application/json', 'mode': "cors"}});
+    console.log(response);
     const data = await response.json();
 
-    console.log(data);
     return data.items.item;
 }
